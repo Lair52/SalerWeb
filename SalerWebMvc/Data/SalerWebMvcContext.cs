fe556c65f719a75fca5febaebf8b04c1ee6f81d5
Lair Anderson <andersonlair3@gmail.com>
2022-03-17 11:20:51 -0300

    CRUD Scaffolding

---
+++ SalerWebMvc/Data/SalerWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalerWebMvc.Models;
+
+namespace SalerWebMvc.Data
+{
+    public class SalerWebMvcContext : DbContext
+    {
+        public SalerWebMvcContext (DbContextOptions<SalerWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalerWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- SalerWebMvc/Startup.cs
+++ SalerWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalerWebMvc.Data;
 
 namespace SalerWebMvc
 {
@@ -33,6 +35,9 @@ namespace SalerWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalerWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalerWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
